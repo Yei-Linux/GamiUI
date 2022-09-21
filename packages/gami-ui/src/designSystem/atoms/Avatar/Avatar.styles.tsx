319c70b938bf345c3204679446562d4f193547c9
Cesar Jesus Alberto Alvan Huarcaya <cesar.alvan@globant.com>
2022-09-20 21:46:24 -0500

    formatting files

--- packages/gami-ui/src/designSystem/atoms/Avatar/Avatar.styles.tsx
+++ packages/gami-ui/src/designSystem/atoms/Avatar/Avatar.styles.tsx
@@ -1,5 +1,5 @@
-import { css } from '@emotion/react'
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 import Row from 'designSystem/layouts/Row'
 import { ICustomTheme } from 'providers/ThemeGamification/ThemeGamification'
 import { mixinFlexVariants } from 'styles/mixins/flex'
